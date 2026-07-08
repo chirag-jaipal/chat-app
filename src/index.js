9801b679eaeeb6f1107e8c1673efa25aa15d3835
Chirag Jaipal <chiragjaipal17@gmail.com>
2026-07-08 03:57:02 +0000

    feat: implement chat message functionality with Socket.IO

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => {
